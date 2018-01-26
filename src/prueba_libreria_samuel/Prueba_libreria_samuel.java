99d9be84f1b9d0b4e7cba6d475201cd80106da88
jvidalmoreira <jvidalmoreira@victoria20.danielcastelao.org>
2018-01-26 09:03:32 +0100

    importar la libreria

---
+++ src/prueba_libreria_samuel/Prueba_libreria_samuel.java
@@ -0,0 +1,11 @@
+
+package prueba_libreria_samuel;
+import com.datos.pedirDatos;
+public class Prueba_libreria_samuel {
+
+
+    public static void main(String[] args) {
+        pedirDatos.leerString("hola");
+    }
+    
+}
